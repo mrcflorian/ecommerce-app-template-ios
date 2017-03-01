ce32f96e8696c6f7ef8cc5a23ce05b40b0da2bb8
Florian Marcu <florian.mrc@gmail.com>
2017-02-28 22:01:18 -0800

    Mock product id changes

--- EcommerceApp/EcommerceApp/Models/Product.swift
+++ EcommerceApp/EcommerceApp/Models/Product.swift
@@ -46,15 +46,15 @@ class Product {
             Product(id: "15", name: "Signature Tailor", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/02/signaturetailoredsuit.jpg", price: 999, description: description, category: "Suits", images: suitImages),
             Product(id: "16", name: "Joseph Slim", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/02/josephslimsuit.jpeg", price: 459.0, description: description, category: "Suits", images: suitImages),
             Product(id: "122", name: "Jack Victor", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/02/jackvictorgibsonsuit.jpg", price: 360, description: description, category: "Suits", images: suitImages),
-            Product(id: "123", name: "Calvin Klein", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/02/calvinkleinsuit.jpg", price: 780, description: description, category: "Suits", images: suitImages),
-            Product(id: "123", name: "Red dress", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/03/dress-1.jpeg", price: 780, description: description, category: "Dresses", images: images),
-            Product(id: "123", name: "Zara", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/03/dress-2.jpeg", price: 74, description: description, category: "Dresses", images: images),
+            Product(id: "12873", name: "Calvin Klein", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/02/calvinkleinsuit.jpg", price: 780, description: description, category: "Suits", images: suitImages),
+            Product(id: "17623", name: "Red dress", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/03/dress-1.jpeg", price: 780, description: description, category: "Dresses", images: images),
+            Product(id: "17623", name: "Zara", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/03/dress-2.jpeg", price: 74, description: description, category: "Dresses", images: images),
             Product(id: "1223", name: "Mohito", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/03/dress-8.jpeg", price: 80, description: description, category: "Dresses", images: images),
             Product(id: "12343", name: "Asos", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/03/dress-4.jpeg", price: 69, description: description, category: "Dresses", images: images),
-            Product(id: "123", name: "Pimkie", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/03/dress-5.jpeg", price: 54, description: description, category: "Dresses", images: images),
-            Product(id: "123", name: "Elie Saab", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/03/dress-6.jpeg", price: 199, description: description, category: "Dresses", images: images),
-             Product(id: "43", name: "H&M", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/03/dress-7.jpeg", price: 47, description: description, category: "Dresses", images: images),
-              Product(id: "45", name: "Victoria Beckham", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/03/dress-8.jpeg", price: 99, description: description, category: "Dresses", images: images),
+            Product(id: "15623", name: "Pimkie", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/03/dress-5.jpeg", price: 54, description: description, category: "Dresses", images: images),
+            Product(id: "16523", name: "Elie Saab", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/03/dress-6.jpeg", price: 199, description: description, category: "Dresses", images: images),
+             Product(id: "4763", name: "H&M", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/03/dress-7.jpeg", price: 47, description: description, category: "Dresses", images: images),
+              Product(id: "455", name: "Victoria Beckham", imageUrl: "http://iosapptemplates.com/wp-content/uploads/2017/03/dress-8.jpeg", price: 99, description: description, category: "Dresses", images: images),
         ]
     }
 }
